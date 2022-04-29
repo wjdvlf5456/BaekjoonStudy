75ba81d9224ee94c483e7f97e0e131690c3d41e6
최정필 <wjdvlf1407@gmail.com>
2022-04-29 19:12:53 +0900

    Q10988 스캐너

--- src/com/baekjoon/study2/Q10988_SJW.java
+++ src/com/baekjoon/study2/Q10988_SJW.java
@@ -11,26 +11,25 @@ public class Q10988_SJW {
 		String word = sc.next();
 
 		int result = 0;
+
+		// for조건문 시작
 		for (int i = 0; i < word.length() / 2; i++) {
 			int sum = 1;
 
 			if (word.charAt(i) == word.charAt(word.length() - 1 - i)) {
-
 				sum = sum + i;
-				System.out.println("if문 sum" + sum);
+
 			} else {
 				sum = 0;
 			}
 
-			System.out.println("for문 sum " + sum);
 			if (sum >= word.length() / 2) {
 				result = 1;
-				System.out.println("if문 속 result: " + result);
 			}
-			System.out.println("for문 result: " + result);
+			//
 
 		}
-
+		// for조건문 끝
 		System.out.println(result);
 
 		sc.close();
