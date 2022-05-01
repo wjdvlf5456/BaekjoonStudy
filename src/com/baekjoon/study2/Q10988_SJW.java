c7be4986f8d4edabed9900defd95ed7a6aacb67e
최정필 <wjdvlf1407@gmail.com>
2022-05-01 21:43:04 +0900

    팰린드롬

--- src/com/baekjoon/study2/Q10988_SJW.java
+++ src/com/baekjoon/study2/Q10988_SJW.java
@@ -16,7 +16,6 @@ public class Q10988_SJW {
 		for (int i = 0; i < word.length() / 2; i++) {
 			int sum = 1;
 
-			// if문 단어가 팰린드롬인지 판별하기
 			if (word.charAt(i) == word.charAt(word.length() - 1 - i)) {
 				sum = sum + i;
 
@@ -24,10 +23,10 @@ public class Q10988_SJW {
 				sum = 0;
 			}
 
-			// if문 맞은 횟수가 단어길이의 절반일 때 팰린드롬이라고 판별함
 			if (sum >= word.length() / 2) {
 				result = 1;
 			}
+			//
 
 		}
 		// for조건문 끝
