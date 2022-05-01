88dab64c586391c3ec63c30211dbf9fb349d522f
최정필 <wjdvlf1407@gmail.com>
2022-05-01 18:05:23 +0900

    홀짝게임 하는중

---
+++ src/com/baekjoon/study2/Q20154_LJH.java
@@ -0,0 +1,23 @@
+package com.baekjoon.study2;
+
+import java.util.Scanner;
+
+public class Q20154_LJH {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		String word = sc.nextLine();
+
+		String[] alphabet = new String[word.length()];
+
+		alphabet[0].charAt(0);
+		
+		System.out.println(alphabet[0]);
+
+		sc.close();
+
+	}
+
+}
