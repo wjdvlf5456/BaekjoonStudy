005a701fdd4c75b7481b20281455e01df35baf3e
최정필 <wjdvlf1407@gmail.com>
2022-05-01 21:40:47 +0900

    스타대회 맞음

--- src/com/baekjoon/study2/Q1057_CBS.java
+++ src/com/baekjoon/study2/Q1057_CBS.java
@@ -8,19 +8,15 @@ public class Q1057_CBS {
 
 		Scanner sc = new Scanner(System.in);
 
-		// 라운드
 		int challenger = sc.nextInt();
 
-		// 김지민
 		int kjm = sc.nextInt();
-		// 임한수
 		int ihs = sc.nextInt();
 
 		int round = 1;
 
 		while (challenger / 2 >= 1) {
 
-			// 둘이 같은 라운드에서 만나는 경우
 			if ((1 + kjm + ihs) % 4 == 0 && (ihs - kjm == 1 || kjm - ihs == 1)) {
 
 				if ((kjm / 2 == (ihs / 2) + 1) || ((kjm / 2) + 1 == ihs / 2)) {
@@ -29,29 +25,22 @@ public class Q1057_CBS {
 				continue;
 
 			} else {
-				// 김지민의 다음라운드 번호
 				if (kjm % 2 == 1) {
 					kjm = (kjm / 2) + 1;
 				} else {
 					kjm = kjm / 2;
 				}
-				System.out.println("김지민: " + kjm);
 
-				// 임한수의 다음라운드 번호
 				if (ihs % 2 == 1) {
 					ihs = (ihs / 2) + 1;
 				} else {
 					ihs = ihs / 2;
 
 				}
-				System.out.println("임한수: " + ihs);
 			}
 
-			// 라운드가 진행될 때 마다 참가자는 절반으로 줄어든다.
 			challenger = challenger / 2;
-			System.out.println("참가자: " + challenger);
 			round += 1;
-			System.out.println("현재라운드: " + round);
 		}
 		System.out.println(round);
 
