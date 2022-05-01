26858bd20a9985832e0b37620de42cfebef50e8d
최정필 <wjdvlf1407@gmail.com>
2022-05-01 19:24:53 +0900

    스타대회 디버깅중

--- src/com/baekjoon/study2/Q1057_CBS.java
+++ src/com/baekjoon/study2/Q1057_CBS.java
@@ -18,30 +18,35 @@ public class Q1057_CBS {
 
 		int round = 1;
 
-		while (challenger / 2 < 1) {
+		while (challenger / 2 > 1) {
 
 			if (kjm / 2 == (ihs / 2) + 1 || (kjm / 2) + 1 == ihs / 2) {
 				break;
-			}
-
-			// 김지민의 다음라운드 번호
-			if (kjm % 2 == 1) {
-				kjm = (kjm / 2) + 1;
 			} else {
-				kjm = kjm / 2;
+				// 김지민의 다음라운드 번호
+				if (kjm % 2 == 1) {
+					kjm = (kjm / 2) + 1;
+				} else {
+					kjm = kjm / 2;
+				}
+				System.out.println("while kjm:  " + kjm);
+
+				// 임한수의 다음라운드 번호
+				if (ihs % 2 == 1) {
+					ihs = (ihs / 2) + 1;
+				} else {
+					ihs = ihs / 2;
+
+				}
+
 			}
 
-			// 임한수의 다음라운드 번호
-			if (ihs % 2 == 1) {
-				ihs = (ihs / 2) + 1;
-			} else {
-				ihs = ihs / 2;
-
-			}
 			challenger = challenger / 2;
+			System.out.println("challenger: " + challenger);
 			round += 1;
 			System.out.println("while문 속 round: " + round);
-		}System.out.println(round);
+		}
+		System.out.println(round);
 
 		sc.close();
 
