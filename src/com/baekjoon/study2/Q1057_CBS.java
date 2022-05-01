3492345a051a67ab55160d2e121c17f182b7bb10
최정필 <wjdvlf1407@gmail.com>
2022-05-01 19:17:15 +0900

    디버깅중

--- src/com/baekjoon/study2/Q1057_CBS.java
+++ src/com/baekjoon/study2/Q1057_CBS.java
@@ -9,22 +9,39 @@ public class Q1057_CBS {
 		Scanner sc = new Scanner(System.in);
 
 		// 라운드
-		int round = sc.nextInt();
+		int challenger = sc.nextInt();
 
 		// 김지민
 		int kjm = sc.nextInt();
 		// 임한수
 		int ihs = sc.nextInt();
 
-		boolean win = true;
+		int round = 1;
 
-		while (round / 2 < 1) {
-			if (kjm % 2 == 1) {
+		while (challenger / 2 < 1) {
 
+			if (kjm / 2 == (ihs / 2) + 1 || (kjm / 2) + 1 == ihs / 2) {
+				break;
 			}
 
-			round /= round / 2;
-		}
+			// 김지민의 다음라운드 번호
+			if (kjm % 2 == 1) {
+				kjm = (kjm / 2) + 1;
+			} else {
+				kjm = kjm / 2;
+			}
+
+			// 임한수의 다음라운드 번호
+			if (ihs % 2 == 1) {
+				ihs = (ihs / 2) + 1;
+			} else {
+				ihs = ihs / 2;
+
+			}
+			challenger = challenger / 2;
+			round += 1;
+			System.out.println("while문 속 round: " + round);
+		}System.out.println(round);
 
 		sc.close();
 
