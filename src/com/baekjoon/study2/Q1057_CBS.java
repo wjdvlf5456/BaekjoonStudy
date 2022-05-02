c1e81cb118b5fc61609f78a126d51977958d617d
최정필 <wjdvlf1407@gmail.com>
2022-05-02 09:46:54 +0900

    토너먼트

--- src/com/baekjoon/study2/Q1057_CBS.java
+++ src/com/baekjoon/study2/Q1057_CBS.java
@@ -16,7 +16,7 @@ public class Q1057_CBS {
 		int round = 1;
 
 		while (challenger / 2 >= 1) {
-
+			// 둘이 같은 라운드에서 만나는 경우
 			if ((1 + kjm + ihs) % 4 == 0 && (ihs - kjm == 1 || kjm - ihs == 1)) {
 
 				if ((kjm / 2 == (ihs / 2) + 1) || ((kjm / 2) + 1 == ihs / 2)) {
@@ -25,12 +25,13 @@ public class Q1057_CBS {
 				continue;
 
 			} else {
+				// 김지민의 다음라운드 번호
 				if (kjm % 2 == 1) {
 					kjm = (kjm / 2) + 1;
 				} else {
 					kjm = kjm / 2;
 				}
-
+				// 임한수의 다음라운드 번호
 				if (ihs % 2 == 1) {
 					ihs = (ihs / 2) + 1;
 				} else {
@@ -38,7 +39,7 @@ public class Q1057_CBS {
 
 				}
 			}
-
+			// 라운드가 진행될 때 마다 참가자는 절반으로 줄어든다.
 			challenger = challenger / 2;
 			round += 1;
 		}
