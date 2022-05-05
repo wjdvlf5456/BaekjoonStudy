f85e62962ce5c6577232ceea5c6a608d9a0753c0
최정필 <wjdvlf1407@gmail.com>
2022-05-05 19:56:39 +0900

    진수 계산기 만드는 중

--- src/com/baekjoon/study03/Q1212_CBS.java
+++ src/com/baekjoon/study03/Q1212_CBS.java
@@ -1,7 +1,26 @@
 package com.baekjoon.study03;
 
+import java.util.Scanner;
+
 public class Q1212_CBS {
-	
-	//8진수 2진수
+
+	public static void main(String[] args) {
+		// 8진수 2진수
+		Scanner sc = new Scanner(System.in);
+
+		String str = sc.nextLine();
+
+		String[] strArray = str.split("");
+
+		int[] eight = new int[strArray.length];
+
+		for (int i = 0, j = strArray.length - 1; i < strArray.length; i++, j--) {
+			eight[i] = Integer.parseInt(strArray[j]);
+
+		}
+
+		sc.close();
+
+	}
 
 }
