c98d8c108a41a5a31447df339c19192ce49d5d89
최정필 <wjdvlf1407@gmail.com>
2022-05-05 21:29:52 +0900

    음계

--- src/com/baekjoon/study03/Q2578_LJH.java
+++ src/com/baekjoon/study03/Q2578_LJH.java
@@ -1,7 +1,45 @@
 package com.baekjoon.study03;
 
+import java.util.Scanner;
+
 public class Q2578_LJH {
-	
-	//빙고
+
+	public static void main(String[] args) {
+
+		// 빙고
+		Scanner sc = new Scanner(System.in);
+
+		String str = sc.nextLine();
+		String[] st = str.split(" ");
+		int[] music = new int[st.length];
+
+		String result = "";
+
+		for (int i = 0; i < music.length; i++) {
+			music[i] = i + 1;
+
+			if (music[i] == Integer.parseInt(st[i])) {
+				if (i == music.length - 1) {
+					System.out.println("ascending");
+				}
+
+				continue;
+
+			} else if (music[i] == Integer.parseInt(st[music.length - i - 1])) {
+				if (i == music.length - 1) {
+					System.out.println("descending");
+				}
+				continue;
+			} else {
+				System.out.println("mixed");
+				break;
+			}
+
+		}
+		System.out.println(result);
+
+		sc.close();
+
+	}
 
 }
