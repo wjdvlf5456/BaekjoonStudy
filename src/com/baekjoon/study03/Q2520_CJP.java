72400f1b85f1d1692e103d5f3abbbb708897f24f
최정필 <wjdvlf1407@gmail.com>
2022-05-05 18:40:25 +0900

    스캐너

--- src/com/baekjoon/study03/Q2520_CJP.java
+++ src/com/baekjoon/study03/Q2520_CJP.java
@@ -15,16 +15,17 @@ public class Q2520_CJP {
 
 		int[] material = new int[mat.length];
 
-		for (int i = 0; i < mat.length; i++) {
-			material[i] = Integer.parseInt(mat[i]);
+		for (int j = 0; j < mat.length; j++) {
+			material[j] = Integer.parseInt(mat[j]);
 		}
 
-		int dough = 1;
+		int dough = 0;
 
 		while (true) {
 
 			if ((material[0] / 8) * (material[1] / 8) * (material[2] / 4) * (material[3] / 1)
 					* (material[4] / 9) == 0) {
+				dough++;
 				break;
 			}
 			material[0] -= 8;
@@ -44,12 +45,12 @@ public class Q2520_CJP {
 
 		int[] topping = new int[toppi.length];
 
-		for (int i = 0; i < toppi.length; i++) {
-			topping[i] = Integer.parseInt(toppi[i]);
+		for (int j = 0; j < toppi.length; j++) {
+			topping[j] = Integer.parseInt(toppi[j]);
 		}
 
 		int sum = topping[0] / 1 + topping[1] / 30 + topping[2] / 25 + topping[3] / 10;
-		if (sum <= dough) {
+		if (sum < dough) {
 			System.out.println(sum);
 
 		} else {
