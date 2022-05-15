b084a7a5c96434aaf7b416e07187975f63a15aff
최정필 <wjdvlf1407@gmail.com>
2022-05-15 23:55:42 +0900

    33

--- src/com/javaex/study06/Q3054_CJP.java
+++ src/com/javaex/study06/Q3054_CJP.java
@@ -1,11 +1,29 @@
 package com.javaex.study06;
 
+import java.util.Scanner;
+
 public class Q3054_CJP {
 
 	public static void main(String[] args) {
-		
-		//피터팬 프레임
-		
+
+		// 피터팬 프레임
+		Scanner sc = new Scanner(System.in);
+
+		String word = sc.nextLine();
+
+		String[] peter = new String[5];
+
+		peter[0] = "..#..";
+		peter[1] = ".#.#.";
+		peter[2] = "#." + word + ".#";
+		peter[3] = ".#.#.";
+		peter[4] = "..#..";
+
+		for (int i = 0; i < peter.length; i++) {
+			System.out.println(peter[i]);
+		}
+
+		sc.close();
 
 	}
 
