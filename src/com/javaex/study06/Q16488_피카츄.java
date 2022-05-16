511efe6e4a95eafbd84b899f818d930c9c4f0311
최정필 <wjdvlf1407@gmail.com>
2022-05-16 14:45:25 +0900

    피카츄

---
+++ src/com/javaex/study06/Q16488_피카츄.java
@@ -0,0 +1,26 @@
+package com.javaex.study06;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q16488_피카츄 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		//int형은 값을 다 담지 못하여 long형 사용
+		long x = Integer.parseInt(st.nextToken());
+		long y = Integer.parseInt(st.nextToken());
+
+		System.out.println(x * x * y);
+
+		br.close();
+
+	}
+
+}
