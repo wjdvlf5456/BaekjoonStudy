68a16f87fda60bd17fbe7cdf57d7f8efb9b62d79
최정필 <wjdvlf1407@gmail.com>
2022-05-19 23:52:05 +0900

    피터팬프레임

--- src/com/baekjoon/study06/Q3054_CJP.java
+++ src/com/baekjoon/study06/Q3054_CJP.java
@@ -3,97 +3,82 @@ package com.baekjoon.study06;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Iterator;
+import java.util.StringTokenizer;
 
 public class Q3054_CJP {
-
-	/* 
-	 * "피터팬 프레임"은 단어를 다이아몬드 형태로 장식하는 것이다. 
-	 * "웬디 프레임"은 피터팬 프레임과 유사하지만, 다이아몬드를 '*'로 만드는 것이다.  
-	 *  
-	 * 단어가 주어졌을 때, 3의 배수 위치(세 번째, 여섯 번째, 아홉번째, ...)에 있는 알파벳은 웬디 프레임으로, 나머지 알파벳은 피터팬 프레임으로 장식 
-	 * 웬디 프레임과 피터팬 프레임이 겹칠 경우에는, 웬디 프레임 
-	 *  
-	 */
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		String str = br.readLine();
-		String[] strArr = str.split("");
+		String frame = br.readLine();
 
-		int n = strArr.length;        // 알파벳 개수
-		int l = 4 * (n - 1) + 5;        // l=4n+1 등차수열 - 한 행 당 출력될 문자 개수
+		int tok = frame.length();
+		String[] peter = new String[5];
+		String[] wendy = new String[5];
 
-		int i;
+		// 피터팬프레임 값
+		peter[0] = "..#.";
+		peter[1] = ".#.#";
+		peter[2] = "#.";
+		peter[3] = ".#.#";
+		peter[4] = "..#.";
 
-		for (i = 1; i < l + 1; i++) {        // 첫번째 줄
-			if ((i + 1) % 4 == 0) {        // 4k-1
-				if ((i + 1) % 12 == 0) {        // 12k-1
-					System.out.print("*");
+		// 웬디프레임 값
+		for (int i = 0; i < 5; i++) {
+			wendy[i] = peter[i].replace("#", "*");
+		}
+
+		for (int i = 0; i < 5; i++) {
+
+			for (int j = 0; j < tok; j++) {
+				// 세번째 줄 출력
+				if (i == 2) {
+					//3의 배수의 위치 앞뒤에 전부 웬디프레임 출력
+					if (j % 3 != 1) {
+						//처음은 무조건 피터팬프레임으로 시작
+						if (j == 0) {
+							System.out.print(peter[i] + frame.charAt(j) + ".");
+						//그 이후 3의 배수의 위치마다 웬디프레임 출력
+						} else {
+							System.out.print(wendy[i] + frame.charAt(j) + ".");
+
+						}
+						//2,5,8...~번째에는 피터팬프레임 출력
+					} else {
+						System.out.print(peter[i] + frame.charAt(j) + ".");
+
+					}
+					//3번째 줄 마지막 값
+					if (j == tok - 1 && frame.length() % 3 == 0) {
+						System.out.print("*");
+					} else if (j == tok - 1)
+						System.out.print("#");
+					
+					//1,2,4,5번째 줄 출력
 				} else {
-					System.out.print("#");
+					if (j % 3 == 2) {
+						System.out.print(wendy[i]);
+					} else {
+						System.out.print(peter[i]);
+
+					}
+
 				}
+			}
+			//3번째 줄에는 공백으로 마무리
+			if (i == 2) {
+				System.out.println("");
+			//나머지 줄은 .으로 마무리
 			} else {
-				System.out.print(".");
+				System.out.println(".");
+
 			}
 		}
-		System.out.println("");
-
-		for (i = 1; i < l + 1; i++) {        // 두번째 줄
-			if (i % 2 == 0) {
-				if ((i + 2) % 12 == 0 || (i) % 12 == 0) {
-					System.out.print("*");
-				} else {
-					System.out.print("#");
-				}
-			} else {
-				System.out.print(".");
-			}
-		}
-		System.out.println("");
-
-		for (i = 1; i < l + 1; i++) {        // 세번째 줄
-			if ((i + 3) % 4 == 0) {
-				if (((i - 1) % 12 == 0 && i > 1) || (i + 3) % 12 == 0) {
-					System.out.print("*");
-				} else {
-					System.out.print("#");
-				}
-			} else if ((i + 1) % 4 == 0) {
-				System.out.print(strArr[((i + 1) / 4) - 1]);
-			} else {
-				System.out.print(".");
-			}
-		}
-		System.out.println("");
-
-		for (i = 1; i < l + 1; i++) {        // 네번째 줄 = 두번째
-			if (i % 2 == 0) {
-				if ((i + 2) % 12 == 0 || (i) % 12 == 0) {
-					System.out.print("*");
-				} else {
-					System.out.print("#");
-				}
-			} else {
-				System.out.print(".");
-			}
-		}
-		System.out.println("");
-
-		for (i = 1; i < l + 1; i++) {        // 다섯번째 줄 (=첫번째)
-			if ((i + 1) % 4 == 0) {
-				if ((i + 1) % 12 == 0) {
-					System.out.print("*");
-				} else {
-					System.out.print("#");
-				}
-			} else {
-				System.out.print(".");
-			}
-		}
-		System.out.println("");
 
 		br.close();
+
 	}
+
 }
