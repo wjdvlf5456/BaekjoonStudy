c6290b7c59bab009d771f2b24330fd5a8d05af97
최정필 <wjdvlf1407@gmail.com>
2022-05-21 15:51:21 +0900

    곰곰이 주석

--- src/com/baekjoon/study08/Q25193_곰곰이의식단관리.java
+++ src/com/baekjoon/study08/Q25193_곰곰이의식단관리.java
@@ -15,26 +15,35 @@ public class Q25193_곰곰이의식단관리 {
 		문자열은 영어 대문자로만 이루어져 있다. S_i가 C인 경우, i번째 음식이 치킨이며, 그 외의 경우에는 다른 음식이다.
 		이 때 곰곰이가 연속으로 치킨을 먹는 날의 최댓값의 최솟값을 구하여라.
 		*/
+		
+		/*
+		연속해서 나오는 값이 최대한 작게 만들려면 최대한 일정하게 나누어 주어야 하기 때문에 치킨을 안 먹는 날을
+		일정한 간격마다 나누어 주면 된다.
+		 */
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		StringTokenizer nst = new StringTokenizer(br.readLine());
+		// 날짜 일 수 입력
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
 
-		int n = Integer.parseInt(nst.nextToken());
-
+		// 치킨이 아닌 경우
 		int notchicken = 1;
 
+		// 무엇을 먹는지 대문자로만 입력
 		String food = br.readLine();
-
 		String[] dFood = food.split("");
 
+		// 치킨을 먹으면 냅두고 치킨이 아니면 notchicken 1씩 추가
 		for (int i = 0; i < n; i++) {
 			if (dFood[i].equals("C")) {
+
 			} else {
 				notchicken++;
 			}
 
 		}
+
 		System.out.println(n / notchicken);
 		br.close();
 	}
