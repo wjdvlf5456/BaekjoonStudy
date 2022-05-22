e8ee0598116ed51627c02e8008888d5608ec1ee5
최정필 <wjdvlf1407@gmail.com>
2022-05-22 19:22:45 +0900

    베스트셀러 주석

--- src/com/baekjoon/study08/Q1302_베스트셀러.java
+++ src/com/baekjoon/study08/Q1302_베스트셀러.java
@@ -18,27 +18,33 @@ public class Q1302_베스트셀러 {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+		// 책 제목 입력횟수
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
 		int n = Integer.parseInt(st.nextToken());
 
+		// 입력할 때 마다 해쉬맵에 데이터 추가
 		for (int i = 0; i < n; i++) {
 			String str = br.readLine();
-
+			// 이전에 있던 키값이면 밸류를 1증가
 			if (bMap.containsKey(str)) {
 				int count = bMap.get(str);
 				bMap.replace(str, count, count + 1);
+
 			} else {
+				// 이전에 없던 키값이면 밸류를 1로 설정 후 해쉬맵에 추가
 				bMap.put(str, 1);
 
 			}
 		}
 
+		// 키는 String으로 밸류는 Integer로 어레이리스트를 사용하여 나누어주었다.
 		List<String> sList = new ArrayList<String>();
 		List<Integer> bList = new ArrayList<Integer>();
 
 		sList.addAll(bMap.keySet());
 		bList.addAll(bMap.values());
+
+		// 최대값
 		int max = 0;
 
 		for (int i = 0; i < bList.size(); i++) {
@@ -47,7 +53,9 @@ public class Q1302_베스트셀러 {
 			}
 
 		}
+		// 최대값 입력 완료
 
+		// 만약 밸류가 최대값보다 적으면 키값도 같이 삭제(삭제되면서 다음에 검사할 값도 배열순서에서 1 줄어들기때문에 i-- 추가)
 		for (int i = 0; i < bList.size(); i++) {
 			if (bList.get(i) < max) {
 				sList.remove(i);
@@ -57,8 +65,11 @@ public class Q1302_베스트셀러 {
 
 			}
 		}
+
+		// 남은 책 제목 정렬
 		sList.sort(null);
 
+		// 맨 앞의 책 제목 출력
 		System.out.println(sList.get(0));
 
 		br.close();
