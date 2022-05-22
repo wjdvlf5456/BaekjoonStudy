a92801621cd73dbdf532ff7367e29a154a83473e
최정필 <wjdvlf1407@gmail.com>
2022-05-22 19:18:32 +0900

    베스트셀러 주석 전

--- src/com/baekjoon/study08/Q1302_베스트셀러.java
+++ src/com/baekjoon/study08/Q1302_베스트셀러.java
@@ -5,14 +5,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Q1302_베스트셀러 {
 
 	public static void main(String[] args) throws IOException {
 
-		List<String> bList = new ArrayList<String>();
+		Map<String, Integer> bMap = new HashMap<String, Integer>();
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -21,17 +23,43 @@ public class Q1302_베스트셀러 {
 		int n = Integer.parseInt(st.nextToken());
 
 		for (int i = 0; i < n; i++) {
-			bList.add(br.readLine());
+			String str = br.readLine();
+
+			if (bMap.containsKey(str)) {
+				int count = bMap.get(str);
+				bMap.replace(str, count, count + 1);
+			} else {
+				bMap.put(str, 1);
+
+			}
 		}
 
-		System.out.println(bList.toString());
+		List<String> sList = new ArrayList<String>();
+		List<Integer> bList = new ArrayList<Integer>();
 
-		bList.sort(null);
-		System.out.println(bList.toString());
-		
-		for (int i = 0; i < args.length; i++) {
+		sList.addAll(bMap.keySet());
+		bList.addAll(bMap.values());
+		int max = 0;
+
+		for (int i = 0; i < bList.size(); i++) {
+			if (bList.get(i) > max) {
+				max = bList.get(i);
+			}
+
 		}
-		
+
+		for (int i = 0; i < bList.size(); i++) {
+			if (bList.get(i) < max) {
+				sList.remove(i);
+				bList.remove(i);
+				i--;
+			} else {
+
+			}
+		}
+		sList.sort(null);
+
+		System.out.println(sList.get(0));
 
 		br.close();
 	}
