7e9cd6738fc42e13c07bfcc73aa2fd617c499a44
최정필 <wjdvlf1407@gmail.com>
2022-05-29 16:32:41 +0900

    파스칼의 삼각형

--- src/com/baekjoon/study10/Q16395_파스칼의삼각형.java
+++ src/com/baekjoon/study10/Q16395_파스칼의삼각형.java
@@ -16,6 +16,24 @@ public class Q16395_파스칼의삼각형 {
 
 		int k = Integer.parseInt(st.nextToken());
 
+		int pascal[][] = new int[n][n];
+
+		pascal[0][0] = 1;
+		pascal[1][0] = 1;
+		pascal[1][1] = 1;
+
+		for (int i = 2; i < n; i++) {
+			for (int j = 0; j <= i; j++) {
+				if (j == 0 || j == i) {
+					pascal[i][j] = 1;
+				} else {
+
+					pascal[i][j] = pascal[i - 1][j - 1] + pascal[i - 1][j];
+
+				}
+			}
+		}
+		System.out.println(pascal[n - 1][k - 1]);
 
 		br.close();
 
