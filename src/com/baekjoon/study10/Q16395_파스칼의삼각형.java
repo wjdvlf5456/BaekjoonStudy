5a869e964fd3265f5346447c96ebabed6c801749
최정필 <wjdvlf1407@gmail.com>
2022-05-29 17:55:18 +0900

    파스칼 리스트 인티저배열추가

--- src/com/baekjoon/study10/Q16395_파스칼의삼각형.java
+++ src/com/baekjoon/study10/Q16395_파스칼의삼각형.java
@@ -1,63 +1,77 @@
 package com.baekjoon.study10;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class Q16395_파스칼의삼각형 {
 
 	public static void main(String[] args) throws IOException {
-		
-		List<Integer[]> triangle = new ArrayList<Integer[]>();
-		
+
+		List<Integer[]> pascal = new ArrayList<Integer[]>();
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 
 		int k = Integer.parseInt(st.nextToken());
 
-		
-		System.out.println(triangle);
-		int pascal[][] = new int[n][n];
+		// ==============================================================================
+		/////////////// 삼각형 맨위 값 1로 지정
+		pascal.add(new Integer[] { 1 });
+		pascal.add(new Integer[] { 1, 1 });
 
+		// 출력 예시
+		/*
+		System.out.println(pascal.get(0)[0]);
+		System.out.println(pascal.get(1)[0]);
+		System.out.println(pascal.get(1)[1]);
+		*/
+
+		// ===================================
+		// 같은 표현
+		/*
+		int pascal[][] = new int[n][n];
+		
 		pascal[0][0] = 1;
 		pascal[1][0] = 1;
 		pascal[1][1] = 1;
-		
-		
+		*/
 
+		// ==============================================================================
+
+		// 이미 지정해놓은 값이 있어 n이 2보다 작으면 1을 출력한다.
 		if (n < 2) {
-			bw.write(pascal[n - 1][k - 1]);
-			bw.flush();
-		} else if(n <= 30 && n >=0){
+			System.out.println(pascal.get(n - 1)[k - 1]);
+		} else if (n <= 30 && n >= 0) {
 
 			for (int i = 2; i < n; i++) {
+				// Integer 배열에 올릴 int배열 생성
+				int[] box = new int[i + 1];
+
 				for (int j = 0; j <= i; j++) {
+					// 양쪽 끝은 항상 1
 					if (j == 0 || j == i) {
-						pascal[i][j] = 1;
+						box[j] = 1;
+						// 그 이외의 값은 이전의 부모값 2개를 합쳐서 int배열에 추가
 					} else {
+						box[j] = pascal.get(i - 1)[j - 1] + pascal.get(i - 1)[j];
 
-						pascal[i][j] = pascal[i - 1][j - 1] + pascal[i - 1][j];
-
-					}
-					if (i == n - 1 && j == k - 1) {
-						String result = Integer.toString(pascal[n - 1][k - 1]);
-						bw.write(result);
-						bw.flush();
-						break;
 					}
 				}
+				// int형 배열을 Integer배열로 바꿔준다.
+				pascal.add(Arrays.stream(box).boxed().toArray(Integer[]::new));
+
 			}
+			// List에서 값 가져오기
+			System.out.println(pascal.get(n - 1)[k - 1]);
 		}
 
-		bw.close();
 		br.close();
 
 	}
