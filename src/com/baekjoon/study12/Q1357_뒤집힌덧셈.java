cd92fccb37ffd8718cff75942996cdd301449ae5
최정필 <wjdvlf1407@gmail.com>
2022-06-03 20:46:18 +0900

    뒤집힌 덧셈 버퍼

--- src/com/baekjoon/study12/Q1357_뒤집힌덧셈.java
+++ src/com/baekjoon/study12/Q1357_뒤집힌덧셈.java
@@ -1,25 +1,37 @@
 package com.baekjoon.study12;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Q1357_뒤집힌덧셈 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int x = sc.nextInt();
-		int y = sc.nextInt();
+		int x = Integer.parseInt(st.nextToken());
+		int y = Integer.parseInt(st.nextToken());
 
 		int result = rev(x) + rev(y);
+		int result2 = rev(result);
+		
+		//int형 String으로 변환
+		bw.write(String.valueOf(result2));
 
-		System.out.println(rev(result));
-
-		sc.close();
+		bw.close();
+		br.close();
 
 	}
 
+	//숫자 역순으로
 	public static int rev(int num) {
+		
 		int sum = 0;
 		while (true) {
 			sum += num % 10;
