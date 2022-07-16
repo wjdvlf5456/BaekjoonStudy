99a3f5262341ef1b7bf07b50e96a1bfba8723db5
최정필 <wjdvlf1407@gmail.com>
2022-07-16 16:26:52 +0900

    시간초과

--- src/com/baekjoon/study21/Q21920_서로소평균.java
+++ src/com/baekjoon/study21/Q21920_서로소평균.java
@@ -17,33 +17,41 @@ public class Q21920_서로소평균 {
 
 		st = new StringTokenizer(br.readLine());
 
+		// n만큼의 길이를 가지는 수열
 		int[] a = new int[n];
 		for (int i = 0; i < n; i++) {
 			a[i] = Integer.parseInt(st.nextToken());
 		}
 
+		// a[]배열의 값들과 비교할 값 X
 		int x = Integer.parseInt(br.readLine());
-		int count = 0;
-		int sum = 0;
+
+		double sum = 0;
+		int numCnt = 0;
 
 		for (int i = 0; i < n; i++) {
-			if (x % a[i] == 0) {
-				count++;
-				sum += a[i];
-				
-			} else if (a[i] % x == 0) {
-				count++;
-				sum += a[i];
+			int count = 0;
+			for (int j = 2; j <= a[i]; j++) {
+				if (a[i] % j == 0 && x % j == 0) {
+					// 최대공약수가 1이 아니면 카운트를 추가
+					count++;
+				}
+
 			}
-		}
-		int aSum = 0;
+			// 카운트가 하나라도 있을 시 서로소가 아니라고 판단한다.
+			if (count < 1) {
+				sum += (double)a[i];
+				numCnt++;
+				//System.out.println("a[" + i + "]: " + a[i]);
+			}
 
-		for (int i = 0; i < n; i++) {
-			aSum += a[i];
 		}
+		
+		//System.out.println("X와 서로수인 수들의 합: " + sum);
+		System.out.println(String.format("%.6f",sum/numCnt));
 
-		double result = (double)((double)(aSum - sum) / (double)(n - count));
-		System.out.println(String.format("%.6f", result));
+		// double result = (double)((double)(aSum - sum) / (double)(n - count));
+		// System.out.println(String.format("%.6f", result));
 
 		br.close();
 
