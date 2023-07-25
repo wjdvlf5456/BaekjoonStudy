34672ef195bc5d4632897abdb573e046a280e9f2
wjdvl <wjdvl@25.20.245.87>
2023-07-25 15:39:43 +0900

    백준 1212

---
+++ src/com/alone/study01/Ex1212_8진수2진수.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Ex1212_8진수2진수 {
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        br.close();
+
+    }
+
+}
